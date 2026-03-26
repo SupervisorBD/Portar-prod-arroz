e7432643d57f752b013f4faf137550b577d07707
SupervisorBD <supervisorbd@grupodieck.hn>
2026-03-26 10:10:35 -0600

    apk v1
    v1

---
+++ data.js
@@ -0,0 +1,480 @@
+const PORTAL_DEFAULT_DATA = {
+  "sections": [
+    {
+      "id": "calderas",
+      "name": "Área de Calderas",
+      "description": "Formatos y monitoreos del área de calderas.",
+      "items": [
+        {
+          "id": "cal-1",
+          "name": "Reporte de Operación de Químicos",
+          "type": "pdf",
+          "url": "docs/PA-FO-04 Reporte de operacion de calderas v01 17-feb 25.pdf",
+          "note": "Reporte operativo del área"
+        },
+        {
+          "id": "cal-2",
+          "name": "Ingreso y Remojo de Materia Prima",
+          "type": "pdf",
+          "url": "docs/PA-FO-15 Control de ingreso y remojo de materia primas v0401 dic25.pdf",
+          "note": "Control de ingreso y remojo"
+        },
+        {
+          "id": "cal-3",
+          "name": "Monitoreo de Dureza del Agua",
+          "type": "pdf",
+          "url": "docs/PA-FO-16 Monitoreo de dureza del agua v02.pdf",
+          "note": "Calidad del agua"
+        },
+        {
+          "id": "cal-4",
+          "name": "Control de Proceso en Planta de Escaldado Semanal",
+          "type": "pdf",
+          "url": "docs/PA-FO-18 Control de proceso planta de escaldado v01.pdf",
+          "note": "Seguimiento semanal"
+        },
+        {
+          "id": "cal-5",
+          "name": "Monitoreo de Inyección de Aire a Silos",
+          "type": "pdf",
+          "url": "docs/PA-FO-19 Monitoreo de inyecion de aire de silos de almacenamiento v01.pdf",
+          "note": "Monitoreo de silos"
+        }
+      ]
+    },
+    {
+      "id": "produccion",
+      "name": "Área de Producción",
+      "description": "Documentos de operación, control y apoyo administrativo.",
+      "items": [
+        {
+          "id": "pro-1",
+          "name": "Formato de Preoperacional",
+          "type": "pdf",
+          "url": "docs/PA-FO-13 Monitoreo Pre-operacional de Planta de Arroz v0601 ene26.pdf",
+          "note": "Chequeo previo de planta"
+        },
+        {
+          "id": "pro-2",
+          "name": "Órdenes de Mantenimiento",
+          "type": "pdf",
+          "url": "docs/GM-FO-21 Solicitud de servicio de mantenimiento v05.pdf",
+          "note": "Solicitud de mantenimiento"
+        },
+        {
+          "id": "pro-3",
+          "name": "Pases de Permisos de Salida",
+          "type": "pdf",
+          "url": "docs/TH-FO-20 Solicitud de permiso y pase de salida v03.pdf",
+          "note": "Control de salida"
+        },
+        {
+          "id": "pro-4",
+          "name": "Asistencias a Capacitaciones",
+          "type": "pdf",
+          "url": "docs/TH-FO-01 Listado de asistencia a capacitaciones v03.pdf",
+          "note": "Registro de asistencia"
+        },
+        {
+          "id": "pro-5",
+          "name": "Ingreso de Visitas a Planta",
+          "type": "pdf",
+          "url": "docs/SH-FO-26 Formato de ingreso de visitas a planta v06 22-ago.pdf",
+          "note": "Control de visitas"
+        },
+        {
+          "id": "pro-6",
+          "name": "Instructivo de Trazabilidad",
+          "type": "pdf",
+          "url": "docs/PA-PR-01 Procedimiento de trazabilidad Arroz v04.pdf",
+          "note": "Procedimiento de trazabilidad"
+        },
+        {
+          "id": "pro-7",
+          "name": "Minutas de Reunión",
+          "type": "pdf",
+          "url": "docs/GC-FO-01 Minuta de Reunión V2.pdf",
+          "note": "Acta de reunión"
+        },
+        {
+          "id": "pro-8",
+          "name": "Listas de Asistencia",
+          "type": "pdf",
+          "url": "docs/GC-FO-08 Lista de asistencia.pdf",
+          "note": "Control de asistencia"
+        },
+        {
+          "id": "pro-9",
+          "name": "Limpieza Profunda",
+          "type": "pdf",
+          "url": "docs/PA-FO-32 Monitoreo de limpieza y sanitización v0601dic25.pdf",
+          "note": "Monitoreo de limpieza"
+        },
+        {
+          "id": "pro-10",
+          "name": "Control de Reprocesos",
+          "type": "pdf",
+          "url": "docs/PA-FO-33 Registro de control de reproceso v03.pdf",
+          "note": "Registro de reproceso"
+        },
+        {
+          "id": "pro-11",
+          "name": "Preparación de Sanitizante",
+          "type": "pdf",
+          "url": "docs/PA-FO-41 Preparación de sanitizantes v0201dic26.pdf",
+          "note": "Preparación de sanitizante"
+        }
+      ]
+    },
+    {
+      "id": "molinos",
+      "name": "Área de Molinos",
+      "description": "Ingresos, controles y rotulación de procesos.",
+      "items": [
+        {
+          "id": "mol-1",
+          "name": "Ingreso de Materia Prima",
+          "type": "pdf",
+          "url": "docs/PA-FO-14 Formato de ingreso de materia prima  v05 may25.pdf",
+          "note": "Recepción de materia prima"
+        },
+        {
+          "id": "mol-2",
+          "name": "Producción y Procesos de Molino Blanco y Escaldado",
+          "type": "pdf",
+          "url": "docs/Registro 1 y 3 unido.pdf",
+          "note": "Registro operativo"
+        },
+        {
+          "id": "mol-3",
+          "name": "Lotificación de Productos",
+          "type": "pdf",
+          "url": "docs/PA-FO-28 Control de lotificación v03-01 ene26.pdf",
+          "note": "Control de lotificación"
+        },
+        {
+          "id": "mol-4",
+          "name": "Control de Pesos",
+          "type": "pdf",
+          "url": "docs/PA-FO-35 Monitoreo de pesos en quintales y arrobas v04.pdf",
+          "note": "Control de pesos"
+        },
+        {
+          "id": "mol-5",
+          "name": "Captación de Metales",
+          "type": "pdf",
+          "url": "docs/PA-FO-36 Formato de captación de imanes v08.pdf",
+          "note": "Captación de metales"
+        },
+        {
+          "id": "mol-6",
+          "name": "Rótulos para Identificación",
+          "type": "link",
+          "url": "https://grupodieckhn-my.sharepoint.com/:x:/g/personal/supervisorbd_grupodieck_hn/IQAw8sxFJGXIT4lqKbDyNeQIAeAYixaXif7DaiQCOAdfZzo?e=iviQjU",
+          "note": "Rótulos y etiquetas"
+        }
+      ]
+    },
+    {
+      "id": "paqueteria",
+      "name": "Área de Paquetería",
+      "description": "Control de lotes, pesos y reportes de producción.",
+      "items": [
+        {
+          "id": "paq-1",
+          "name": "Lotificación de Productos",
+          "type": "pdf",
+          "url": "docs/PA-FO-28 Control de lotificación v03-01 ene26.pdf",
+          "note": "Asignación de lote"
+        },
+        {
+          "id": "paq-2",
+          "name": "Monitoreo de Pesos",
+          "type": "pdf",
+          "url": "docs/PA-FO-37 Monitoreo de pesos paqueteria v02.pdf",
+          "note": "Pesos de paquetería"
+        },
+        {
+          "id": "paq-3",
+          "name": "Reporte de Producción de Paquetería",
+          "type": "pdf",
+          "url": "docs/Registro 02 y 08.pdf",
+          "note": "Producción diaria"
+        },
+        {
+          "id": "paq-4",
+          "name": "Rótulos para Identificación",
+          "type": "link",
+          "url": "https://grupodieckhn-my.sharepoint.com/:x:/g/personal/supervisorbd_grupodieck_hn/IQAw8sxFJGXIT4lqKbDyNeQIAeAYixaXif7DaiQCOAdfZzo?e=iviQjU",
+          "note": "Rótulos compartidos"
+        },
+        {
+          "id": "paq-5",
+          "name": "Etiquetas de Desperdicios de Plástico",
+          "type": "link",
+          "url": "https://grupodieckhn-my.sharepoint.com/:x:/r/personal/supervisorbd_grupodieck_hn/_layouts/15/Doc.aspx?sourcedoc=%7BA406ADA3-16C7-4C03-AAE5-E403A30687BB%7D&file=etiqueta%20angel.xlsx&action=default&mobileredirect=true&DefaultItemOpen=1",
+          "note": "Etiqueta Excel"
+        }
+      ]
+    },
+    {
+      "id": "requisas",
+      "name": "Requisas y Solicitudes",
+      "description": "Monitoreos virtuales, solicitudes y formularios.",
+      "items": [
+        {
+          "id": "req-1",
+          "name": "Excel de Requisición",
+          "type": "excel",
+          "url": "https://grupodieckhn-my.sharepoint.com/:x:/r/personal/supervisorbd_grupodieck_hn/_layouts/15/Doc.aspx?sourcedoc=%7BAF583290-4179-45AD-A4A2-F9072320ED64%7D&file=GS-FO-10%20Requisici%C3%B3n%20V03%201.xlsx&action=default&mobileredirect=true&DefaultItemOpen=1",
+          "note": "Requisición en Excel"
+        },
+        {
+          "id": "req-2",
+          "name": "Solicitud de Material de Empaque",
+          "type": "form",
+          "url": "https://forms.office.com/pages/responsepage.aspx?id=iX0MKcI4DU6Hn985LEggUs3gJd3qXxpMtk9PlhHfj29UNllPMDAzMElTTTNKTU9WWVdWN0JSR1pCUi4u&origin=lprLink&route=shorturl",
+          "note": "Formulario de empaque"
+        },
+        {
+          "id": "req-3",
+          "name": "Solicitud de Repuestos de Bodega",
+          "type": "form",
+          "url": "https://forms.office.com/pages/responsepage.aspx?id=iX0MKcI4DU6Hn985LEggUmvspen6D4hClx4JZ4hlMgpURTZOS09BSU04MjZQMDVXTk5XOFA2U0JIMi4u&origin=lprLink&route=shorturl",
+          "note": "Formulario de repuestos"
+        },
+        {
+          "id": "req-4",
+          "name": "Solicitud de Insumos de Oficina",
+          "type": "form",
+          "url": "https://forms.office.com/pages/responsepage.aspx?id=iX0MKcI4DU6Hn985LEggUucEjphlwmVKm6clc6GrLyJUOEk0NDRIVkFWQzAxRlVUR0QyMFJPME5ZVS4u&origin=lprLink&route=shorturl",
+          "note": "Formulario de oficina"
+        },
+        {
+          "id": "req-5",
+          "name": "Solicitud de Mantenimiento",
+          "type": "form",
+          "url": "https://forms.monday.com/forms/c1ffc16d6319b9e6bbf270f084f52817?r=use1",
+          "note": "Requerimiento de mantenimiento"
+        },
+        {
+          "id": "req-6",
+          "name": "Solicitud para Infraestructura y Seguridad Industrial",
+          "type": "form",
+          "url": "https://forms.monday.com/forms/4182ddb8ee115d52d61344278ebdc806?r=use1",
+          "note": "Infraestructura y seguridad"
+        }
+      ]
+    },
+    {
+      "id": "personal",
+      "name": "Área de Personal",
+      "description": "Control de horas, asistencia y registros de personal.",
+      "items": [
+        {
+          "id": "per-1",
+          "name": "Planilla de Horas Extras",
+          "type": "excel",
+          "url": "https://grupodieckhn-my.sharepoint.com/:x:/r/personal/supervisorbd_grupodieck_hn/_layouts/15/Doc.aspx?sourcedoc=%7BF42BE0DD-6CD2-4457-A383-C5F4C73450C9%7D&file=Control%20HE_2026.xlsx&action=default&mobileredirect=true&DefaultItemOpen=1",
+          "note": "Planilla HE 2026"
+        },
+        {
+          "id": "per-2",
+          "name": "Asistencias Diarias",
+          "type": "excel",
+          "url": "https://grupodieckhn-my.sharepoint.com/:x:/r/personal/ariel_clavasquin_grupodieck_hn/_layouts/15/Doc.aspx?sourcedoc=%7B5D6D3E1C-43CC-47C8-BA1C-943B286DC998%7D&file=LISTADO%20DE%20ASISTENCIA%202026%201.xlsx&fromShare=true&action=default&mobileredirect=true",
+          "note": "Lista diaria"
+        }
+      ]
+    },
+    {
+      "id": "cierre",
+      "name": "Área de Cierre de Proceso",
+      "description": "Liberaciones, programación y plataformas de secadas y paros.",
+      "items": [
+        {
+          "id": "cie-1",
+          "name": "Liberaciones de Productos Terminados",
+          "type": "excel",
+          "url": "https://grupodieckhn-my.sharepoint.com/:x:/r/personal/supervisorbd_grupodieck_hn/_layouts/15/Doc.aspx?sourcedoc=%7B58C70987-C3FF-48C9-814A-B11DF85958D9%7D&file=CA-FO-05%20Registro%20de%20liberaci%C3%B3n%20de%20producto%202026%20(34).xlsx&action=default&mobileredirect=true&DefaultItemOpen=1",
+          "note": "Liberación de producto"
+        },
+        {
+          "id": "cie-2",
+          "name": "Programaciones Diarias",
+          "type": "excel",
+          "url": "https://grupodieckhn-my.sharepoint.com/:x:/r/personal/supervisorbd_grupodieck_hn/_layouts/15/Doc.aspx?sourcedoc=%7B6FC1DD0B-624F-4DE1-B0FE-CFDD2F353578%7D&file=PROGRAMACI%C3%93N%20PED_ZN%20ARROZ%20V1.xlsb&action=default&mobileredirect=true",
+          "note": "Programación diaria"
+        },
+        {
+          "id": "cie-3",
+          "name": "Plataforma de Ingreso de Secadas",
+          "type": "link",
+          "url": "http://192.168.10.11:805/Planta/ReportProdPaqueteria?ECBC5BA15ABB5ECB53347FAE305B14DA69FF1CD9AD7DA0703788EE4E53DAB841041BC620F00531471EBFEC1A56E74F14467164476590177845FBF48F89CC0847=TB39EECF23B124FEF849B6A11FCA6DDD9",
+          "note": "Ingreso de secadas"
+        },
+        {
+          "id": "cie-4",
+          "name": "Plataforma de Ingreso de Paros de Escaldado",
+          "type": "link",
+          "url": "https://supervisorbd.github.io/ESCALDADO-PAROS/",
+          "note": "Ingreso de paros"
+        },
+        {
+          "id": "cie-5",
+          "name": "Cortes de Producción Diarios",
+          "type": "excel",
+          "url": "https://grupodieckhn-my.sharepoint.com/:x:/g/personal/supervisorbd_grupodieck_hn/IQBupGzY2xVYTrUgsP3UBrn-Ac52YS41pwm_ME3mj-pvXF4?e=Yw1zyU",
+          "note": "Corte diario"
+        }
+      ]
+    },
+    {
+      "id": "indicadores-diarios",
+      "name": "Actualizaciones de Indicadores Diarios",
+      "description": "Seguimiento diario de reprocesos y OEE.",
+      "items": [
+        {
+          "id": "ind-1",
+          "name": "Reprocesos",
+          "type": "excel",
+          "url": "https://grupodieckhn-my.sharepoint.com/:x:/g/personal/alejandra_murillo_grupodieck_hn/IQCEhTbRtU4rRrzajvV1MrExAUC_cKDR9Yfezjch-ieX-eQ?e=uZ2ImQ",
+          "note": "Indicador diario"
+        },
+        {
+          "id": "ind-2",
+          "name": "OEE Paquetería",
+          "type": "excel",
+          "url": "https://grupodieckhn.sharepoint.com/:x:/r/sites/DieckDigital/_layouts/15/Doc.aspx?sourcedoc=%7B04D3B92D-7003-485D-B5EC-A4A271E34C91%7D&file=OEE.xlsx&fromShare=true&action=default&mobileredirect=true",
+          "note": "Eficiencia de paquetería"
+        }
+      ]
+    },
+    {
+      "id": "agujas",
+      "name": "Monitoreo Diario de Agujas",
+      "description": "Validación y formulario del monitoreo diario de agujas.",
+      "items": [
+        {
+          "id": "agu-1",
+          "name": "Validación de Datos",
+          "type": "excel",
+          "url": "https://grupodieckhn-my.sharepoint.com/:x:/g/personal/supervisorbd_grupodieck_hn/IQCtgFwMi3VcRaXqIOPQ8iIFAWMXTp-kx7NktNsw_y15ssM?e=rFdNiX",
+          "note": "Validación diaria"
+        },
+        {
+          "id": "agu-2",
+          "name": "Formulario",
+          "type": "form",
+          "url": "https://forms.office.com/r/2dy14iwavf",
+          "note": "Captura del monitoreo"
+        }
+      ]
+    },
+    {
+      "id": "tarimas",
+      "name": "Monitoreo de Tarimas",
+      "description": "Control y captura de tarimas.",
+      "items": [
+        {
+          "id": "tar-1",
+          "name": "Validación de Datos",
+          "type": "excel",
+          "url": "",
+          "note": "Pendiente de enlace"
+        },
+        {
+          "id": "tar-2",
+          "name": "Formulario",
+          "type": "form",
+          "url": "",
+          "note": "Pendiente de enlace"
+        }
+      ]
+    },
+    {
+      "id": "agua",
+      "name": "Monitoreo de Agua en Proceso",
+      "description": "Validación de datos y formulario del proceso de agua.",
+      "items": [
+        {
+          "id": "aguap-1",
+          "name": "Validación de Datos",
+          "type": "excel",
+          "url": "https://grupodieckhn-my.sharepoint.com/:x:/r/personal/supervisorbd_grupodieck_hn/_layouts/15/Doc.aspx?sourcedoc=%7B6C2F8F60-6D23-4AA4-9DFC-0734AABC78E2%7D&file=MONITOREO%20DE%20AGUA%20DE%20PROCESO.xlsx&action=edit&mobileredirect=true&wdMsFormsCorrelationId=74488ff7-7ca4-4349-a749-d252df7cb4bb&wdtf=%20Microsoft.Office.Excel.FMsFormsMetadataInWorkbookMetadata%3Atrue",
+          "note": "Datos de proceso"
+        },
+        {
+          "id": "aguap-2",
+          "name": "Formulario",
+          "type": "form",
+          "url": "https://forms.office.com/r/5a335VB5b5",
+          "note": "Captura de agua"
+        }
+      ]
+    },
+    {
+      "id": "producto",
+      "name": "Monitoreo de Producto en Proceso",
+      "description": "Control de datos y formulario de producto en proceso.",
+      "items": [
+        {
+          "id": "prod-1",
+          "name": "Validación de Datos",
+          "type": "excel",
+          "url": "https://grupodieckhn-my.sharepoint.com/:x:/r/personal/supervisorbd_grupodieck_hn/_layouts/15/Doc.aspx?sourcedoc=%7BF703A76F-1E32-4109-BAD4-D524D724A61E%7D&file=Registro%20Producto%20en%20%20%20Proceso%204.xlsx&action=edit&mobileredirect=true&wdMsFormsCorrelationId=af28cbca-4001-4c68-9c22-adf4d5f6454a&wdtf=%20Microsoft.Office.Excel.FMsFormsMetadataInWorkbookMetadata%3Atrue",
+          "note": "Datos de proceso"
+        },
+        {
+          "id": "prod-2",
+          "name": "Formulario",
+          "type": "form",
+          "url": "https://forms.office.com/r/BMg4hCZYiD",
+          "note": "Captura de producto"
+        }
+      ]
+    },
+    {
+      "id": "tarimas-planta",
+      "name": "Monitoreo de Tarimas en Planta",
+      "description": "Control de tarimas en planta y su formulario.",
+      "items": [
+        {
+          "id": "tarp-1",
+          "name": "Validación de Datos",
+          "type": "excel",
+          "url": "https://grupodieckhn-my.sharepoint.com/:x:/r/personal/ariel_clavasquin_grupodieck_hn/_layouts/15/Doc.aspx?sourcedoc=%7B62E164CF-72FF-44D9-9366-D44061144A92%7D&file=CONTROL%20DE%20TARIMAS%20EN%20PLANTA..xlsx&fromShare=true&action=default&mobileredirect=true",
+          "note": "Control de tarimas"
+        },
+        {
+          "id": "tarp-2",
+          "name": "Formulario",
+          "type": "form",
+          "url": "https://forms.office.com/pages/responsepage.aspx?id=iX0MKcI4DU6Hn985LEggUld0_21KqUZIr-XJtXaebx5UM1BJMFNGREg0QzNWWU02VklCUERCM0pJNC4u&origin=lprLink&route=shorturl",
+          "note": "Formulario de tarimas"
+        }
+      ]
+    },
+    {
+      "id": "indicadores-quincenales",
+      "name": "Actualizaciones de Indicadores Quincenales",
+      "description": "Indicadores quincenales para análisis y seguimiento.",
+      "items": [
+        {
+          "id": "quin-1",
+          "name": "Vidrio y Plástico Quebradizo",
+          "type": "excel",
+          "url": "https://grupodieckhn-my.sharepoint.com/:x:/g/personal/supervisorbd_grupodieck_hn/IQC4X7pM_rhBR5DTBOkDvl0EAcAmP_5RUgklXazk2X56VrM?e=f2txIw",
+          "note": "Indicador quincenal"
+        },
+        {
+          "id": "quin-2",
+          "name": "KPI de Producción",
+          "type": "excel",
+          "url": "https://grupodieckhn-my.sharepoint.com/:x:/r/personal/ariel_clavasquin_grupodieck_hn/_layouts/15/Doc.aspx?sourcedoc=%7BF783807F-B1F3-491A-9E5F-330B5F170176%7D&file=KPI%20PRODUCCION.xlsx&action=default&mobileredirect=true&DefaultItemOpen=1",
+          "note": "KPI general"
+        }
+      ]
+    }
+  ]
+};
